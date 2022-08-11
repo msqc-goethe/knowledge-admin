65cddec9cd194aa102c2608cf0211b70799b9012
Cubii <jannik.pflieger@gmx.de>
2022-08-11 23:14:36 +0200

    nb card doesnt work

--- src/app/pages/io500/io500.component.ts
+++ src/app/pages/io500/io500.component.ts
@@ -2,17 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
 
 @Component({
-  selector: 'io500-app',
+  selector: 'app-io500',
   templateUrl: './io500.component.html',
 })
-export class IO500Component implements OnInit{
+export class IO500Component{
 
-  constructor(private theme: NbThemeService) {
-    this.theme.getJsTheme();
-}
-
-
-  ngOnInit(): void {
-    
-  }
 }
