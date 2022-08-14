bb51207b80f990598324f6c27218989ae3d5874d
Cubii <jannik.pflieger@gmx.de>
2022-08-14 14:46:32 +0200

    edited testchart

--- src/app/pages/io500/chart.component.ts
+++ src/app/pages/io500/chart.component.ts
@@ -11,18 +11,27 @@ import { Result} from '../../webservice.service';
 })
 export class ChartTestComponent {
 
-data = {
-  labels: ["January", "February", "March", "April"],
-  datasets: [
-    {
-      label: "My First dataset",
-      data: [65, 59, 80, 81]
-    }
-  ]
-};
-options = {
-  maintainAspectRatio: false
-};
+    data = {
+        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+        datasets: [{
+          data: [65, 59, 90, 81, 56, 55, 40],
+          label: 'Series A',
+        }, {
+          data: [28, 48, 40, 19, 96, 27, 100],
+          label: 'Series B',
+        }],
+      };
+      options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scaleFontColor: 'white',
+    
+        scale: {
+          pointLabels: {
+            fontSize: 14,
+          },
+        },
+      };
 
 
 }
